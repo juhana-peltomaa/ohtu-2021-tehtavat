c87b432814719ec5b246ef26e45b3669a6c6a071
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-02-17 21:26:52 +0200

    Viikko 1 - teht 16 - testi 100%

--- koodi/viikko1/nhl-statistics-1/src/tests/nhl_test.py
+++ koodi/viikko1/nhl-statistics-1/src/tests/nhl_test.py
@@ -2,16 +2,18 @@ import unittest
 from statistics import Statistics
 from player import Player
 
+players = [
+    Player("Semenko", "EDM", 4, 12),
+    Player("Lemieux", "PIT", 45, 54),
+    Player("Kurri",   "EDM", 37, 53),
+    Player("Yzerman", "DET", 42, 56),
+    Player("Gretzky", "EDM", 35, 89)
+]
+
 
 class PlayerReaderStub:
     def get_players(self):
-        return [
-            Player("Semenko", "EDM", 4, 12),
-            Player("Lemieux", "PIT", 45, 54),
-            Player("Kurri",   "EDM", 37, 53),
-            Player("Yzerman", "DET", 42, 56),
-            Player("Gretzky", "EDM", 35, 89)
-        ]
+        return players
 
 
 class TestStatistics(unittest.TestCase):
@@ -19,6 +21,24 @@ class TestStatistics(unittest.TestCase):
         # annetaan Statistics-luokan oliolle "stub"-luokan olio
         self.statistics = Statistics(PlayerReaderStub())
 
-    def test_konstuktori_asettaa_pelaajan_oikein(self):
-        self.assertAlmostEqual(self.statistics.top_scorers(
-            1), "Gretzky EDM 35 + 89 = 124")
+    def test_haku(self):
+        player = self.statistics.search("Gretzky")
+        self.assertEqual(player, players[-1])
+
+    def test_haku_vaara_nimi(self):
+        player = self.statistics.search("Gretzky2")
+        self.assertEqual(player, None)
+
+    def test_points(self):
+        from statistics import sort_by_points
+
+        points = sort_by_points(players[-1])
+        self.assertEqual(points, 124)
+
+    def test_top_score(self):
+        player = self.statistics.top_scorers(1)
+        self.assertEqual(player[0], players[-1])
+
+    def test_team(self):
+        player = self.statistics.team("PIT")
+        self.assertEqual(player[0], players[1])
